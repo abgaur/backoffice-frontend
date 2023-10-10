4c1cdb4f9d494cd5bdedb939162748bd65bcb956
Abhishek Gaur <abhishekgaur61@gmail.com>
2023-10-10 16:10:51 -0700

    CI CD demo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h2>testing CI CD11</h2>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+        <div>
+            <h1>This is a new header</h1>
+            <h2>Another header</h2>
+        </div>
+    )
+}
+
+export default Header;
