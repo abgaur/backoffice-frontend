0d3ee1817d24bde6db56f88804e178b6d1ae6de5
Abhishek Gaur <abhishekgaur61@gmail.com>
2023-11-02 17:07:23 -0700

    add navbar

--- src/ui-components/common/navbar.jsx
+++ src/ui-components/common/navbar.jsx
@@ -1,10 +1,37 @@
 import React, { useState, useEffect } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 
 const Navbar = () => {
 
   return (
-    <h1>this is navbar</h1>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
+                    <Link className="nav-link active" to="/products">Products</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link active" to="/">Home</Link>
+                </li>  
+                <li className="nav-item">
+                    <Link className="nav-link active" to="/productdetails">Product Details</Link>
+                </li>  
+            </ul>
+            </div>
+        </div>
+    </nav>
   );
 };
 
